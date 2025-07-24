7906bec5f57b882d1c2a6ff6c589126e80311d88
VikasKandhal <140228156+VikasKandhal@users.noreply.github.com>
2025-07-24 11:20:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#Encryption Program
+
+import random
+import string
+
+chars =" " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#print(f"Chars : {chars}")
+#print(f"Key : {key}")
+
+
+#ENCRYPT
+
+plain_text = input("Enter a message to encrypt:")
+cipher_text = ""
+
+for letter in plain_text:
+  index = chars.index(letter)
+  cipher_text += key[index]
+
+print(f"Original Message:{plain_text}")
+print(f"Encrypted Message:{cipher_text}")
+
+#DECRYPT
+
+cipher_text_text = input("Enter a message to decrypt:")
+plane_text = ""
+
+for letter in cipher_text:
+  index = key.index(letter)
+  plane_text += chars[index]
+
+print(f"Encrypted Message:{cipher_text}")
+print(f"OriginalMessage:{plain_text}")
+
